f6c8f7b493294af837b77621f676b3b5dfa83eec
Sofia Carolina Rojas Flores <sofia.rojasflo@gmail.com>
2021-05-29 19:23:21 -0500

    updated comments on api route file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,8 @@
-// LOAD DATA
-// Linking the routes to the date
 
-//const noteData = require('../data/noteData');
+// Require data
 const fs = require('fs');
 const path = require("path");
-//const { read } = require('../data/noteData')
 
-let theNotes;
 
 // ROUTING
 module.exports = (app) => {
@@ -14,14 +10,15 @@ module.exports = (app) => {
    // API notes reads the db.json file
    const noteData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
 
+  // API GET Request that gets the response
    app.get('/api/notes', (req, res) => res.json(noteData));
 
-   // API POST Request returns all saved notes
+   // API POST Request returns all saved notes on the request body
    app.post('/api/notes', (req, res) => {
       noteData.push(req.body);
       res.json(true);
    });
-   
+   // Use FS WriteFileSync to write the notes
    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(noteData),
    function (err, data) {
       if (err) {
